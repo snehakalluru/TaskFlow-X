1de3e0932dab568110fb6a6639a933a90721a4de
snehakalluru <snehakalluru91@gmail.com>
2026-06-28 20:42:01 +0530

    deployment issues

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
